b1b91b48e3b77f874106d650907de33460713ad6
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-26 23:31:26 +0530

    Updated error-handler.ts middleware to double check the error status code assuming it as a custom error class instance - done to capture error thrown by error handlers in base-auth-handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -7,14 +7,29 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  // Check if the error is an instance of custom error class.
   if (err instanceof CustomError) {
+    // If it is, handle it by sending a response with the serialized errors and the specified status code.
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  // If the error is not a instance of CustomError Base Class, then Console it for debugging purposes.
-  console.error(err);
+  // The below line of code will capture the statusCode in errors,
+  // IF they are thrown by error handlers in base-auth-handler module.
+  // It'll try accessing the statusCode property of the error considering it as a instance of CustomError,
+  // If it dosen't exist, it'll default to 400 (Bad Request).
+  const statusCode = (err as CustomError)?.statusCode || 400;
 
-  res.status(400).send({
+  if (!(err as CustomError)?.statusCode){
+
+    // If err.statusCode does not exist,
+    // The error is not an instance of custom error class.
+    // Log it for debugging purposes.
+    console.error(err);
+
+  }
+
+  // Send a response with the error message and the determined status code.
+  res.status(statusCode).send({
     errors: [{ message: err.message }],
   });
 };
