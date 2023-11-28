b2e99b0c76a77855caf00ecddd459f366b0aa5e7
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-29 00:18:57 +0530

    Fixed a typo in the comments.

--- src/common-error-classes/database-connection-error.ts
+++ src/common-error-classes/database-connection-error.ts
@@ -7,7 +7,7 @@ export class DatabaseConnectionError extends CustomError {
     // Calling the parent class (Error) constructor as this is a constructor of sub-class
     super("Error connecting to database!!!");
 
-    // Since we are extending a built-in class (Error) the following line of cade has to be added
+    // Since we are extending a built-in class (Error) the following line of code has to be added
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 
